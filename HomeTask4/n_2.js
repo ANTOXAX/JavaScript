1c0cc0906eaeb8ce0ddc8bc27de3c7325b4648c6
Streltsov Anton Timofeevich <antoxanet99@gmail.com>
2017-10-13 00:15:05 +0300

    ДЗ_4_2

--- HomeTask4/n_2.js
+++ HomeTask4/n_2.js
@@ -1,4 +1,4 @@
 var num = prompt("Enter telephone number");
 var arrnum = num.match(/\+(375)\s(33|25|29|44)\s(\d\d\d)-(\d\d)-(\d\d)/);
-if ()  console.log(arrnum[1]+arrnum[2]+arrnum[3]+arrnum[4]+arrnum[5]);
+if (arrnum != null)  console.log(arrnum[1]+arrnum[2]+arrnum[3]+arrnum[4]+arrnum[5]);
 else console.log("This number does not exist.");
